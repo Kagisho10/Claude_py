98fb53e3acab3ca454c8ddb361ef59e00293296e
kaygee Ma-ribe <116413856+Kagisho10@users.noreply.github.com>
2025-02-17 23:44:01 +0200

    Add files via upload
    Removed the API_Key

---
+++ Claude.py
@@ -0,0 +1,25 @@
+import os
+from anthropic import Anthropic
+
+client = Anthropic(
+    api_key=os.environ.get(""),  # This is the default and can be omitted
+)
+
+message = client.messages.create(
+    max_tokens=1024,
+    messages=[
+        {
+            "role": "user",
+            "content": "Hello, Claude",
+        }
+    ],
+    model="claude-3-5-sonnet-latest",
+)
+print(message.content)
+
+
+
+
+
+
+
